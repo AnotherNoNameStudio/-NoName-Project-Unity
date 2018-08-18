51e9d6d77db55e69e4f2500fad82be7e7c51b96a
Damian Ubowski <sverda300@gmail.com>
2018-08-18 09:43:26 +0200

    Fading when player enters and leaves the main scene

--- Assets/Scripts/SceneProceeder.cs
+++ Assets/Scripts/SceneProceeder.cs
@@ -1,15 +1,26 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneProceeder : MonoBehaviour
 {
     public string DestinationSceneName;
+    public Image Black;
+    public Animator Animator;
 
     void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.CompareTag("Player"))
         {
-            SceneManager.LoadScene(DestinationSceneName);
+            StartCoroutine(Fading());
         }
+    }
+
+    IEnumerator Fading()
+    {
+        Animator.SetBool("Fade", true);
+        yield return new WaitUntil(() => Black.color.a == 1);
+        SceneManager.LoadScene(DestinationSceneName);
     }
 }
