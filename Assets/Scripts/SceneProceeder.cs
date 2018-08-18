4d146fe308c46d000d6e542bf934bb5a637c50fd
Damian Ubowski <sverda300@gmail.com>
2018-08-18 09:05:02 +0200

    Enter to new scene if player steps on portal

---
+++ Assets/Scripts/SceneProceeder.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneProceeder : MonoBehaviour
+{
+    public string DestinationSceneName;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(DestinationSceneName);
+        }
+    }
+}
