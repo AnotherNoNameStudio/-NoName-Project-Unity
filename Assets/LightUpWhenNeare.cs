3cb2635587d76cb31accfd14d3557526f6f6e9f6
Damian Ubowski <sverda300@gmail.com>
2018-08-20 21:05:19 +0200

    Light is up when player is near to door

---
+++ Assets/LightUpWhenNeare.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightUpWhenNeare : MonoBehaviour
+{
+    public Light Light;
+    public Transform Player;
+    public float MinimumLightDistance;
+    private Transform _door;
+    private float _startLightIndensity;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _door = gameObject.GetComponent<Transform>();
+	    _startLightIndensity = Light.intensity;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float distance = Vector3.Distance(Player.position, _door.position);
+        if (distance < MinimumLightDistance)
+        {
+            float percentDistance =  distance / MinimumLightDistance;
+            Light.intensity = (1f - percentDistance) * _startLightIndensity;
+        }
+        else
+        {
+            Light.intensity = 0;
+        }
+	}
+}
