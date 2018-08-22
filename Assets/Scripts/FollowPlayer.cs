e85aff16a07c689aa8195ed6930815ad5a30bb8e
BlackSkyNight <matejczyk.mateusz@gmail.com>
2018-08-22 21:10:05 +0200

    Tilemap, PerfectPixel camera, new sprite settings, fixed 'bleeding bcg'

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform followed;
+    private Vector3 offset;
+    
+
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - followed.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void LateUpdate()
+    {
+        transform.position = followed.transform.position + offset;
+    }
+}
